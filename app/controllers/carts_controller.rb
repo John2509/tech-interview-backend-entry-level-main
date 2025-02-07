9548e2bd9da624b4cc4461dac4adbf95c87dfec5
João Frezzato <jmarcelofo@gmail.com>
2025-02-07 22:25:28 +0000

    Initial cart routes

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,3 +1,18 @@
 class CartsController < ApplicationController
-  ## TODO Escreva a lógica dos carrinhos aqui
+  # GET /cart
+  def show
+    @cart = Cart.last
+    render json: @cart
+  end
+
+  # POST /cart
+  def create
+    @cart = Cart.new(total_price: 0)
+
+    if @cart.save
+      render json: @cart, status: :created, location: @cart
+    else
+      render json: @cart.errors, status: :unprocessable_entity
+    end
+  end
 end
